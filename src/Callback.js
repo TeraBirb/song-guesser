b86869c7d2fe84a9ffd05d0095f2f1fcfa5747dd
Isaiah Callano <isaiah.g.callano@gmail.com>
2023-05-17 15:13:02 -0700

    pseudocode

--- src/Callback.js
+++ src/Callback.js
@@ -1,4 +1,12 @@
 const Callback = () => {
+
+// Window, score on top, middle is song 1 out of 5, play button, can only play
+// and replay
+// multiple choice bottom 4 choices, random strings from lyrics
+// or random strings from other song titles?
+// Play function, checks if there is no current game
+// iterate 5 times
+
     return (  
         <div className="callback">
             <h1>Guess it worked</h1>
